acc-coin/acc-save-purchase

Upgrade SDK 2.1.0

packages/contracts/test/StorePurchase.test.ts
@@ -1,6 +1,6 @@
+import { Block, BlockHeader, Hash, hashFull, Utils } from "acc-save-purchase-sdk";
 import * as assert from "assert";
 import chai, { expect } from "chai";
-import { Block, BlockHeader, Hash, hashFull, Utils } from "acc-save-purchase-sdk";
 import { solidity } from "ethereum-waffle";
 import { BigNumber } from "ethers";
 import { ethers, upgrades, waffle } from "hardhat";

bosagora/dms-save-purchase

packages/contracts/test/StorePurchase.test.ts
@@ -1,6 +1,6 @@
 import * as assert from "assert";
 import chai, { expect } from "chai";
-import { Block, BlockHeader, Hash, hashFull, Utils } from "dms-store-purchase-sdk";
+import { Block, BlockHeader, Hash, hashFull, Utils } from "dms-save-purchase-sdk";
 import { solidity } from "ethereum-waffle";
 import { BigNumber } from "ethers";
 import { ethers, upgrades, waffle } from "hardhat";